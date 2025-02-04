b075e8ee6491fcf3ab0722af1741c6badffe5aa2
demiurgeQuantified <8355611+demiurgeQuantified@users.noreply.github.com>
2025-02-04 01:09:53 +0000

    bump to 0.1.2

--- src/com/github/zomboiddecompiler/ZomboidDecompiler.java
+++ src/com/github/zomboiddecompiler/ZomboidDecompiler.java
@@ -24,7 +24,7 @@ public class ZomboidDecompiler {
 
     public static final int VERSION_MAJOR = 0;
     public static final int VERSION_MINOR = 1;
-    public static final int VERSION_PATCH = 1;
+    public static final int VERSION_PATCH = 2;
 
     private boolean copyDependencies = false;
 
