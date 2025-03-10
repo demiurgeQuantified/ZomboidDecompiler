650b493f459918013dc66855b2abcc7e71bc9d79
demiurgeQuantified <8355611+demiurgeQuantified@users.noreply.github.com>
2025-03-10 20:44:53 +0000

    fix regression where rosetta data could not be loaded from resources

--- src/main/java/com/github/zomboiddecompiler/rosetta/RosettaPlugin.java
+++ src/main/java/com/github/zomboiddecompiler/rosetta/RosettaPlugin.java
@@ -4,10 +4,10 @@ import org.jetbrains.annotations.Nullable;
 import org.jetbrains.java.decompiler.api.plugin.Plugin;
 import org.jetbrains.java.decompiler.api.plugin.PluginOptions;
 import org.jetbrains.java.decompiler.main.DecompilerContext;
+import org.jetbrains.java.decompiler.main.extern.IFernflowerLogger;
 import org.jetbrains.java.decompiler.main.extern.IVariableNamingFactory;
 import org.jetbrains.java.decompiler.util.Pair;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -15,9 +15,10 @@ import java.net.URL;
 import java.nio.file.*;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.Stream;
 
 public class RosettaPlugin implements Plugin {
+    private FileSystem fileSystem = null;
+
     @Override
     public String id() {
         return "rosetta";
@@ -25,7 +26,7 @@ public class RosettaPlugin implements Plugin {
 
     @Override
     public String description() {
-        return "Renames method parameters using data from a Rosetta file.";
+        return "Renames method parameters using data from Rosetta files.";
     }
 
     @Override
@@ -35,10 +36,41 @@ public class RosettaPlugin implements Plugin {
             return null;
         }
 
-        Path rosettaPath;
+        Path rosettaPath = resolveRosettaPath(rosettaDir);
+        if (rosettaPath == null) {
+            DecompilerContext.getLogger().writeMessage("Rosetta path did not resolve to a valid directory. Ignoring.", IFernflowerLogger.Severity.WARN);
+            return null;
+        }
 
-        if (rosettaDir.startsWith("$")) {
-            URL rosettaURL = getClass().getResource(rosettaDir.substring(1));
+        RosettaParser parser = new RosettaParser();
+        parser.parseDirectory(rosettaPath);
+
+        cleanup();
+
+        return new RosettaNamingFactory(parser.namespaces);
+    }
+
+    // hack to prevent the file system from being closed when the rosetta parser tries to read it
+    // TODO i hate this!!! resource cleanup should not be manual
+    // how can this be restructured for try-with-resources to make sense?
+    private void cleanup() {
+        if (fileSystem != null) {
+            try {
+                fileSystem.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    @Override
+    public @Nullable PluginOptions getPluginOptions() {
+        return () -> Pair.of(RosettaPluginOptions.class, RosettaPluginOptions::addDefaults);
+    }
+
+    private @Nullable Path resolveRosettaPath(String directory) {
+        if (directory.startsWith("$")) {
+            URL rosettaURL = getClass().getClassLoader().getResource(directory.substring(1));
             if (rosettaURL == null) {
                 return null;
             }
@@ -49,41 +81,23 @@ public class RosettaPlugin implements Plugin {
                 // this seems stupid and wasn't necessary before, but as soon as i moved to gradle, it is?
                 Map<String, String> env = new HashMap<>();
                 env.put("create", "true");
-                try(FileSystem ignored = FileSystems.newFileSystem(uri, env)) {
-                    rosettaPath = Paths.get(uri);
+                fileSystem = FileSystems.newFileSystem(uri, env);
+
+                Path rosettaPath = Paths.get(uri);
+                if (!Files.exists(rosettaPath) || !Files.isDirectory(rosettaPath)) {
+                    return null;
                 }
+                return rosettaPath;
             } catch (URISyntaxException | IOException e) {
                 e.printStackTrace();
                 return null;
             }
         } else {
-            rosettaPath = Paths.get(rosettaDir);
-        }
-
-        Path jsonFolder = rosettaPath.resolve("json");
-
-        assert Files.exists(jsonFolder);
-
-        RosettaParser parser = new RosettaParser();
-
-        try (Stream<Path> files = Files.walk(jsonFolder)) {
-            for (Path file : files.toList()) {
-                if (!file.getFileName().toString().toLowerCase().endsWith(".json")) continue;
-                try {
-                    parser.parseJson(Files.newInputStream(file));
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                }
+            Path rosettaPath = Paths.get(directory);
+            if (!Files.exists(rosettaPath) || !Files.isDirectory(rosettaPath)) {
+                return null;
             }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+            return rosettaPath;
         }
-
-        return new RosettaNamingFactory(parser.namespaces);
-    }
-
-    @Override
-    public @Nullable PluginOptions getPluginOptions() {
-        return () -> Pair.of(RosettaPluginOptions.class, RosettaPluginOptions::addDefaults);
     }
 }
