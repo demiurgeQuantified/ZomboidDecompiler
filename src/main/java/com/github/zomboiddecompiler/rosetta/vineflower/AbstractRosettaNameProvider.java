b27678932d32caf3b9ecd17992dd9c51ff1eabd7
demiurgeQuantified <8355611+demiurgeQuantified@users.noreply.github.com>
2025-08-19 21:59:47 +0100

    add zombie to reserved variable names to avoid potential package name conflicts

--- src/main/java/com/github/zomboiddecompiler/rosetta/vineflower/AbstractRosettaNameProvider.java
+++ src/main/java/com/github/zomboiddecompiler/rosetta/vineflower/AbstractRosettaNameProvider.java
@@ -16,7 +16,53 @@ import java.util.Set;
 
 /// Base class for Rosetta name providers.
 public abstract class AbstractRosettaNameProvider implements IVariableNameProvider {
-    private final static Set<String> primitiveTypeNames = Set.of(
+    @Override
+    public void addParentContext(IVariableNameProvider renamer) {
+
+    }
+
+    /**
+     * Assigns good names to variables based on their type.
+     * @param variables The variables to assign names for.
+     * @param invalidNames Names that should not be used. This is best used to avoid giving names already taken in that scope.
+     * @return Map of variables to their new names.
+     */
+    protected Map<VarVersionPair, String> assignUnknownVariableNames(Map<VarVersionPair, VarType> variables,
+                                                                     @Nullable Set<String> invalidNames) {
+        Map<VarVersionPair, String> result = new LinkedHashMap<>();
+
+        // map of variables by their string type name
+        // the string is used so that type names that end up the same will share an id space
+        Map<String, List<VarVersionPair>> variableTypeMap = new LinkedHashMap<>();
+        for (var entry : variables.entrySet()) {
+            String typeName = getDefaultVariableName(entry.getValue());
+            variableTypeMap.putIfAbsent(typeName, new ArrayList<>());
+            variableTypeMap.get(typeName).add(entry.getKey());
+        }
+
+        for (var entry : variableTypeMap.entrySet()) {
+            List<VarVersionPair> vars = entry.getValue();
+            String typeName = entry.getKey();
+
+            if (vars.size() == 1) {
+                if (AbstractRosettaNameProvider.reservedNames.contains(typeName)) {
+                    typeName = typeName + 0;
+                }
+                result.put(vars.get(0), getValidName(typeName, invalidNames));
+                continue;
+            }
+
+            for (int i = 0; i < vars.size(); i++) {
+                String name = getValidName(typeName + i, invalidNames);
+                result.put(vars.get(i), name);
+            }
+        }
+
+        return result;
+    }
+
+    /// Names that should never be used as variable names without a number suffix.
+    private final static Set<String> reservedNames = Set.of(
             "byte",
             "short",
             "int",
@@ -24,8 +70,13 @@ public abstract class AbstractRosettaNameProvider implements IVariableNameProvid
             "float",
             "double",
             "boolean",
-            "char"
+            "char",
+            "zombie"
     );
+
+    private static boolean invalidTypeNameProviderWarned = false;
+
+    private final static ITypeNameProvider DEFAULT_NAME_PROVIDER = new DefaultTypeNameProvider();
 
     /**
      * Ensures the passed name is a valid name for Java code, returning it if it is or a new valid name if it isn't.
@@ -59,54 +110,5 @@ public abstract class AbstractRosettaNameProvider implements IVariableNameProvid
             }
         }
         return DEFAULT_NAME_PROVIDER.nameVar(type);
-    }
-
-    /**
-     * Assigns good names to variables based on their type.
-     * @param variables The variables to assign names for.
-     * @param invalidNames Names that should not be used. This is best used to avoid giving names already taken in that scope.
-     * @return Map of variables to their new names.
-     */
-    protected Map<VarVersionPair, String> assignUnknownVariableNames(Map<VarVersionPair, VarType> variables,
-                                                                     @Nullable Set<String> invalidNames) {
-        Map<VarVersionPair, String> result = new LinkedHashMap<>();
-
-        // map of variables by their string type name
-        // the string is used so that type names that end up the same will share an id space
-        Map<String, List<VarVersionPair>> variableTypeMap = new LinkedHashMap<>();
-        for (var entry : variables.entrySet()) {
-            String typeName = getDefaultVariableName(entry.getValue());
-            variableTypeMap.putIfAbsent(typeName, new ArrayList<>());
-            variableTypeMap.get(typeName).add(entry.getKey());
-        }
-
-        for (var entry : variableTypeMap.entrySet()) {
-            List<VarVersionPair> vars = entry.getValue();
-            String typeName = entry.getKey();
-
-            if (vars.size() == 1) {
-                if (primitiveTypeNames.contains(typeName)) {
-                    typeName = typeName + 0;
-                }
-                result.put(vars.get(0), getValidName(typeName, invalidNames));
-                continue;
-            }
-
-            for (int i = 0; i < vars.size(); i++) {
-                String name = getValidName(typeName + i, invalidNames);
-                result.put(vars.get(i), name);
-            }
-        }
-
-        return result;
-    }
-
-    private static boolean invalidTypeNameProviderWarned = false;
-
-    private final static ITypeNameProvider DEFAULT_NAME_PROVIDER = new DefaultTypeNameProvider();
-
-    @Override
-    public void addParentContext(IVariableNameProvider renamer) {
-
     }
 }
