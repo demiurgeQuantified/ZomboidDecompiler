f687af54a891b6905a09e921aafbbb9f24bd8624
albion <8355611+demiurgeQuantified@users.noreply.github.com>
2026-08-09 04:34:08 +0100

    Bump to v0.3.2

--- src/main/java/com/github/zomboiddecompiler/ZomboidDecompiler.java
+++ src/main/java/com/github/zomboiddecompiler/ZomboidDecompiler.java
@@ -26,7 +26,7 @@ public class ZomboidDecompiler {
 
     public static final int VERSION_MAJOR = 0;
     public static final int VERSION_MINOR = 3;
-    public static final int VERSION_PATCH = 1;
+    public static final int VERSION_PATCH = 2;
 
     /// Whether to create a jar file containing all detected source files.
     private boolean jarGame = false;
