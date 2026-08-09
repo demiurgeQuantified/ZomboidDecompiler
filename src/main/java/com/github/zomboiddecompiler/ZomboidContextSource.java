b91b83774e970873b199c50770cf00f87c006637
albion <8355611+demiurgeQuantified@users.noreply.github.com>
2026-08-09 04:29:06 +0100

    Remove unused import

--- src/main/java/com/github/zomboiddecompiler/ZomboidContextSource.java
+++ src/main/java/com/github/zomboiddecompiler/ZomboidContextSource.java
@@ -8,7 +8,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.FileSystem;
-import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
